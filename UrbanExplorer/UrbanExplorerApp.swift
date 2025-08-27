5acf3d6e3f4dbbdfc15648c9c7f0aa88f8e355f6
Михаил Тихонов <mishkatix88@gmail.com>
2025-08-27 15:51:56 +0300

    Initial Commit

---
+++ UrbanExplorer/UrbanExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  UrbanExplorerApp.swift
+//  UrbanExplorer
+//
+//  Created by Михайло Тихонов on 27.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct UrbanExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
