4e32ee3306e5e13e03920e050745d9f452dac2b2
Михаил Тихонов <mishkatix88@gmail.com>
2025-08-27 22:17:12 +0300

    Build MainMapView UI and connect to ViewModel

--- MainMapView.swift
+++ MainMapView.swift
@@ -6,10 +6,52 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MainMapView: View {
+    @State private var viewModel = ViewModel()
+    
+    let startPosition = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 50.4501, longitude: 30.5234),
+            span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        )
+    )
+    
     var body: some View {
-        Text("Map Screen")
+        NavigationStack {
+            MapReader { proxy in
+                Map(initialPosition: startPosition) {
+                    ForEach(viewModel.spots) { spot in
+                        Annotation(spot.name, coordinate: spot.coordinate) {
+                            Image(systemName: "star.circle")
+                                .resizable()
+                                .foregroundStyle(.orange)
+                                .frame(width: 38, height: 38)
+                                .background(.white)
+                                .clipShape(.circle)
+                                .onTapGesture {
+                                    viewModel.selectedSpot = spot
+                                }
+                                
+                        }
+                    }
+                }
+                .mapStyle(viewModel.mapStyle)
+                .onTapGesture { position in
+                    if let coordinate = proxy.convert(position, from: .local) {
+                        viewModel.addSpot(at: coordinate)
+                    }
+                }
+                .toolbar {
+                    Button("Map Style") {
+                        viewModel.isHybrid.toggle()
+                    }
+                }
+                
+            }
+            .navigationTitle("Urban Explorer")
+        }
     }
 }
 
