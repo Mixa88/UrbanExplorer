f122df5954c88295b531cf9312e673a65d6fd3b4
Михаил Тихонов <mishkatix88@gmail.com>
2025-08-29 15:57:27 +0300

    feat: Implement delete functionality
    - Added deleteSpot() method to MainMapView.ViewModel
    - Updated EditSpotView with an onDelete closure
    - Added a "Delete" button to the EditSpotView UI

---
+++ View/EditSpotView.swift
@@ -0,0 +1,18 @@
+//
+//  EditSpotView.swift
+//  UrbanExplorer
+//
+//  Created by Михайло Тихонов on 29.08.2025.
+//
+
+import SwiftUI
+
+struct EditSpotView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditSpotView()
+}
---
+++ ViewModel/EditSpotView-ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  EditSpotView-ViewModel.swift
+//  UrbanExplorer
+//
+//  Created by Михайло Тихонов on 29.08.2025.
+//
+
+import Foundation
