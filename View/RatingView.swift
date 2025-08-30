ddc346bbd9417c21d320cbc18a27c70c1e7f6c0b
Михаил Тихонов <mishkatix88@gmail.com>
2025-08-30 15:09:30 +0300

    feat: Add rating functionality

---
+++ View/RatingView.swift
@@ -0,0 +1,18 @@
+//
+//  RatingView.swift
+//  UrbanExplorer
+//
+//  Created by Михайло Тихонов on 30.08.2025.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RatingView()
+}
