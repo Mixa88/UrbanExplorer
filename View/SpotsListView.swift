f45b3b59a7d21025b6d8343aff8ccc814755b532
Михаил Тихонов <mishkatix88@gmail.com>
2025-08-31 10:27:15 +0300

    refactor: Centralize ViewModel in AppTabView for shared state

---
+++ View/SpotsListView.swift
@@ -0,0 +1,18 @@
+//
+//  SpotsListView.swift
+//  UrbanExplorer
+//
+//  Created by Михайло Тихонов on 31.08.2025.
+//
+
+import SwiftUI
+
+struct SpotsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpotsListView()
+}
